b80c8a0c0d2e747c2cb6150cdc4840b7b1ccce17
RAMITA LAPTOP <you@example.com>
2026-05-11 23:10:16 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Footer from './component/Footer'
+import Center from './component/Center'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+      <Center/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Center.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Center = () => {
+  return (
+    <div>
+      This is center section
+    </div>
+  )
+}
+
+export default Center
+
+
+
